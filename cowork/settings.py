87f10825d51f2ef88d39010f35abaa6d81180942
hinakhadim <hina.khadim@arbisoft.com>
2024-09-21 17:53:22 +0500

    fix: add awss s3 storage

--- cowork/settings.py
+++ cowork/settings.py
@@ -102,18 +102,18 @@ WSGI_APPLICATION = 'cowork.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT'),
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     "NAME": os.environ.get('DB_NAME'),
+    #     "USER": os.environ.get('DB_USER'),
+    #     "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
+    #     "HOST": os.environ.get('DB_HOST'),
+    #     "PORT": os.environ.get('DB_PORT'),
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 

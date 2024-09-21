8b938a82ff30b36133972835e6fb4846deac5e92
hinakhadim <hina.khadim@arbisoft.com>
2024-09-21 18:27:28 +0500

    fix: add cors

--- cowork/settings.py
+++ cowork/settings.py
@@ -161,7 +161,7 @@ AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ACCESS_KEY_ID=os.environ.get('AWS_S3_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY=os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
 AWS_S3_REGION_NAME=os.environ.get('AWS_S3_REGION_NAME')
-AWS_DEFAULT_ACL='public-read'
+AWS_DEFAULT_ACL=None
 AWS_S3_ENDPOINT_URL=os.environ.get('AWS_S3_ENDPOINT_URL')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
